94ef74c45911cfd0b5be59b6520a0338755752ea
juanfelipe153 <131912651+juanfelipe153@users.noreply.github.com>
2026-08-22 19:40:23 -0500

    corrections

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 
 
-const initialcards = [
+const initialCards = [
     {name: "Valle de Yosemite", link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"},
     {name: "Lago Louise", link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"},
     {name: "Montañas Calvas", link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"},
@@ -17,14 +17,14 @@ initialcards.forEach(function(card){
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close');
 const editPopupModal = document.querySelector('#edit-popup');
-let formElement = document.querySelector('.popup__form');
+const formElement = document.querySelector('.popup__form');
 
 function openModal(modal){
-    modal.classList.add('popup_opened');    
+    modal.classList.add('popup_is-opened');    
 }
 
 function closeModal(modal){
-    modal.classList.remove('popup_opened');
+    modal.classList.remove('popup_is-opened');
 }
 
 editButton.addEventListener('click', function(){
@@ -53,8 +53,15 @@ function handleOpenEditModal(){
 function handleProfileFormSubmit(evt){
     evt.preventDefault();
 
-    let nameInput = document.querySelector('.popup__input_type_name');
-    let descriptionInput = document.querySelector('.popup__input_type_description');
+    const nameInput = document.querySelector('.popup__input_type_name');
+    const descriptionInput = document.querySelector('.popup__input_type_description');
+    const profileTitle = document.querySelector('.profile__title');
+    const profileDescription = document.querySelector('.profile__description');
+
+    profileTitle.textContent = nameInput.value;
+    profileDescription.textContent = descriptionInput.value;
+
+    closeModal(editPopupModal);
 
 };
 
